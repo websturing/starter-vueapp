aa462ca70cbc3b221e27418bfa967ab77f58f5b5
websturing <websturing.project@gmail.com>
2025-07-24 04:30:23 +0000

    [Style] Cursor pointer in child menu

--- src/layouts/AdminLayout.vue
+++ src/layouts/AdminLayout.vue
@@ -12,49 +12,12 @@
                     target="_blank">
                     <img src="../assets/img/logo-ct-dark.png" width="26px" height="26px" class="navbar-brand-img h-100"
                         alt="main_logo">
-                    <span class="ms-1 font-weight-bold">Kai Tim</span>
+                    <span class="ms-1 font-weight-bold">{{ meta?.app_name || "Vue App" }}</span>
                 </a>
             </div>
             <hr class="horizontal dark mt-0">
             <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
                 <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a :class="['nav-link', routeModules?.module == 'Dashboard' ? 'active' : '']"
-                            :href="$router.resolve({ name: 'dashboard' }).href"
-                            @click.prevent="$router.push({ name: 'dashboard' })">
-                            <div
-                                class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                                <i class="icon icon-home-2 text-dark text-sm opacity-10"></i>
-                            </div>
-                            <span class="nav-link-text ms-1">Dashboard</span>
-                        </a>
-                    </li>
-                    <li class="nav-item mt-3">
-                        <h6 class="ps-4 ms-2 text-sm opacity-6">Applications Settings
-                        </h6>
-                    </li>
-                    <li class="nav-item">
-                        <a :class="['nav-link', routeModules?.module == 'Permissions' ? 'active' : '']"
-                            :href="$router.resolve({ name: 'permissions' }).href"
-                            @click.prevent="$router.push({ name: 'permissions' })">
-                            <div
-                                class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                                <i class="icon icon-blockchain-3 text-dark text-sm opacity-10"></i>
-                            </div>
-                            <span class="nav-link-text ms-1">Permission</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a :class="['nav-link', routeModules?.module == 'Users' ? 'active' : '']"
-                            :href="$router.resolve({ name: 'users' }).href"
-                            @click.prevent="$router.push({ name: 'users' })">
-                            <div
-                                class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                                <i class="icon icon-profile text-dark text-sm opacity-10"></i>
-                            </div>
-                            <span class="nav-link-text ms-1">Users</span>
-                        </a>
-                    </li>
                     <template v-for="item in permissions || []" :key="item.id">
                         <li class="nav-item" v-if="item.children.length == 0">
                             <a :class="['nav-link', routeModules?.module === capitalize(item.slug) ? 'active' : '']"
@@ -80,10 +43,11 @@
                         <template v-if="Array.isArray(item?.children) && item.children.length">
                             <li class="nav-item" v-for="child in item.children || []" :key="child.id">
                                 <a :class="['nav-link', routeModules?.module === capitalize(child?.slug) ? 'active' : '']"
+                                    style="cursor:pointer"
                                     @click.prevent="child?.slug && $router.push({ name: child.slug })">
                                     <div
                                         class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                                        <i class="icon icon-profile text-dark text-sm opacity-10"></i>
+                                        <i :class="[child.icon, 'text-dark text-sm opacity-10 icon']"></i>
                                     </div>
                                     <span class="nav-link-text ms-1">
                                         {{ child?.name || 'Unnamed' }} <!-- Fallback text -->
@@ -94,7 +58,6 @@
                     </template>
                 </ul>
             </div>
-            <pre>{{ permissions }}</pre>
             <div class="sidenav-footer mx-3 ">
                 <a href="https://www.creative-tim.com/learning-lab/bootstrap/license/argon-dashboard" target="_blank"
                     class="btn btn-dark btn-sm w-100 mb-3">Documentation</a>
@@ -129,7 +92,7 @@
                             <li class="nav-item d-flex align-items-center">
                                 <a href="javascript:;" class="nav-link text-white font-weight-bold px-0">
                                     <i class="fa fa-user me-sm-1"></i>
-                                    <span class="d-sm-inline d-none">Sign In</span>
+                                    <span class="d-sm-inline d-none">{{ user?.email }}</span>
                                 </a>
                             </li>
                             <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
@@ -140,95 +103,6 @@
                                         <i class="sidenav-toggler-line bg-white"></i>
                                     </div>
                                 </a>
-                            </li>
-                            <li class="nav-item px-3 d-flex align-items-center">
-                                <a href="javascript:;" class="nav-link text-white p-0">
-                                    <i class="fa fa-cog fixed-plugin-button-nav cursor-pointer"></i>
-                                </a>
-                            </li>
-                            <li class="nav-item dropdown pe-2 d-flex align-items-center">
-                                <a href="javascript:;" class="nav-link text-white p-0" id="dropdownMenuButton"
-                                    data-bs-toggle="dropdown" aria-expanded="false">
-                                    <i class="fa fa-bell cursor-pointer"></i>
-                                </a>
-                                <ul class="dropdown-menu  dropdown-menu-end  px-2 py-3 me-sm-n4"
-                                    aria-labelledby="dropdownMenuButton">
-                                    <li class="mb-2">
-                                        <a class="dropdown-item border-radius-md" href="javascript:;">
-                                            <div class="d-flex py-1">
-                                                <div class="my-auto">
-                                                    <img src="../assets/img/team-2.jpg" class="avatar avatar-sm  me-3 ">
-                                                </div>
-                                                <div class="d-flex flex-column justify-content-center">
-                                                    <h6 class="text-sm font-weight-normal mb-1">
-                                                        <span class="font-weight-bold">New message</span> from Laur
-                                                    </h6>
-                                                    <p class="text-xs text-secondary mb-0">
-                                                        <i class="fa fa-clock me-1"></i>
-                                                        13 minutes ago
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </li>
-                                    <li class="mb-2">
-                                        <a class="dropdown-item border-radius-md" href="javascript:;">
-                                            <div class="d-flex py-1">
-                                                <div class="my-auto">
-                                                    <img src="../assets/img/small-logos/logo-spotify.svg"
-                                                        class="avatar avatar-sm bg-gradient-dark  me-3 ">
-                                                </div>
-                                                <div class="d-flex flex-column justify-content-center">
-                                                    <h6 class="text-sm font-weight-normal mb-1">
-                                                        <span class="font-weight-bold">New album</span> by Travis Scott
-                                                    </h6>
-                                                    <p class="text-xs text-secondary mb-0">
-                                                        <i class="fa fa-clock me-1"></i>
-                                                        1 day
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a class="dropdown-item border-radius-md" href="javascript:;">
-                                            <div class="d-flex py-1">
-                                                <div class="avatar avatar-sm bg-gradient-secondary  me-3  my-auto">
-                                                    <svg width="12px" height="12px" viewBox="0 0 43 36" version="1.1"
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                        xmlns:xlink="http://www.w3.org/1999/xlink">
-                                                        <title>credit-card</title>
-                                                        <g stroke="none" stroke-width="1" fill="none"
-                                                            fill-rule="evenodd">
-                                                            <g transform="translate(-2169.000000, -745.000000)"
-                                                                fill="#FFFFFF" fill-rule="nonzero">
-                                                                <g transform="translate(1716.000000, 291.000000)">
-                                                                    <g transform="translate(453.000000, 454.000000)">
-                                                                        <path class="color-background"
-                                                                            d="M43,10.7482083 L43,3.58333333 C43,1.60354167 41.3964583,0 39.4166667,0 L3.58333333,0 C1.60354167,0 0,1.60354167 0,3.58333333 L0,10.7482083 L43,10.7482083 Z"
-                                                                            opacity="0.593633743"></path>
-                                                                        <path class="color-background"
-                                                                            d="M0,16.125 L0,32.25 C0,34.2297917 1.60354167,35.8333333 3.58333333,35.8333333 L39.4166667,35.8333333 C41.3964583,35.8333333 43,34.2297917 43,32.25 L43,16.125 L0,16.125 Z M19.7083333,26.875 L7.16666667,26.875 L7.16666667,23.2916667 L19.7083333,23.2916667 L19.7083333,26.875 Z M35.8333333,26.875 L28.6666667,26.875 L28.6666667,23.2916667 L35.8333333,23.2916667 L35.8333333,26.875 Z">
-                                                                        </path>
-                                                                    </g>
-                                                                </g>
-                                                            </g>
-                                                        </g>
-                                                    </svg>
-                                                </div>
-                                                <div class="d-flex flex-column justify-content-center">
-                                                    <h6 class="text-sm font-weight-normal mb-1">
-                                                        Payment successfully completed
-                                                    </h6>
-                                                    <p class="text-xs text-secondary mb-0">
-                                                        <i class="fa fa-clock me-1"></i>
-                                                        2 days
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </li>
-                                </ul>
                             </li>
                         </ul>
                     </div>
@@ -247,6 +121,7 @@ import { getModulesFromRoute } from '@/lib/routeParser';
 import { useApplicationStore } from '@/stores/application';
 import { useAuthStore } from '@/stores/auth';
 import { push } from 'notivue';
+import { storeToRefs } from 'pinia';
 import { ConfirmDialog } from 'primevue';
 import { useConfirm } from "primevue/useconfirm";
 import { computed, onMounted } from 'vue';
@@ -260,6 +135,9 @@ const route = useRoute();
 const router = useRouter();
 
 const routeModules = computed(() => getModulesFromRoute(route));
+const { email, user } = storeToRefs(authStore)
+const { meta } = storeToRefs(appStore)
+
 const confirm1 = () => {
     confirm.require({
         message: 'Are you sure you want to proceed?',
